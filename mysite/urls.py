a111d850b2f641db0a92be5b858340a921d72e99
kimsangho <sanghokim00@naver.com>
2020-03-22 22:39:32 +0900

    modified login views

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,7 +20,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^accounts/login/$', auth_views.login, name = 'login'),
+    url(r'^accounts/login/$', auth_views.LoginView(templated_name = 'registration/login.html'), name = 'login'),
     url(r'^accounts/logout/$', auth_views.logout, name = 'logout', kwargs = {'next_page' : '/'}),
     url(r'', include('blog.urls')),
 ]
