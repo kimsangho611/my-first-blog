67e5702348b2235acae761d073a377136b4f7d38
kimsangho <sanghokim00@naver.com>
2020-03-22 22:00:19 +0900

    Added login/logout views

--- blog/views.py
+++ blog/views.py
@@ -3,18 +3,22 @@ from django.shortcuts import redirect
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
+from django.contrib.auth.decorators import login_required
 # from 뒤의 .(점)은 현재 디렉토리 혹은 애플리케이션을 의미한다.
 
+@login_required
 def post_list(request):
     posts = Post.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts' : posts})
 
 # 세부 url에 대한 페이지를 위한 함수
+@login_required
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk = pk)
     return render(request, 'blog/post_detail.html', {'post' : post})
 
 # form을 저장하기 위한 함수
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -29,6 +33,7 @@ def post_new(request):
         return render(request, 'blog/post_edit.html', {'form': form})
 
 # form을 검증하기 위한 함수
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -42,3 +47,9 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
         return render(request, 'blog/post_edit.html', {'form': form})
+
+@login_required
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk = pk)
+    post.delete()
+    return redirect('post_list')
